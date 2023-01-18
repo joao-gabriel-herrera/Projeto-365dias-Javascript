b0f686a839cc9d4b3d84802a6fd9a32f897f091b
João Herrera <joaogherrera@hotmail.com>
2023-01-18 15:47:06 -0300

    Projeto 365 dias de javascript

--- Javascript_dia35.js
+++ Javascript_dia35.js
@@ -1,2 +1,19 @@
 // Projeto código nosso de cada dia (dia 35)
 // Segurança virtual
+const verificarEntrada = () => {
+  nomeConvidado = document.getElementById("nome").value;
+  convidadosJoao = [
+    "amanda",
+    "sabrina",
+    "rafael",
+    "dalben",
+    "pedro",
+    "marcelo",
+    "josiane",
+  ];
+  convidadosJoao.includes(nomeConvidado.toLowerCase())
+    ? (document.getElementById("permissaoDeEntrada").innerText =
+        "Você pode entrar!")
+    : (document.getElementById("permissaoDeEntrada").innerText =
+        "Você não pode entrar!");
+};
