f597db8ac88e1b88db871b75fe8f64c32c133f72
João Herrera <joaogherrera@hotmail.com>
2023-01-24 23:52:48 -0300

    Projeto 365 dias de javascript

--- Javascript_dia36.js
+++ Javascript_dia36.js
@@ -1,20 +1,8 @@
 // Projeto código nosso de cada dia (dia 36)
 // Função que verifica palíndromo
 function palindrome(str) {
-  let string = str.replace(/ /g, "");
-  let stringdois = string.split("");
-  stringdois = stringdois.map((p) => p.toLowerCase());
-  let inv = stringdois;
-  inv.reverse();
-  if (stringdois.length !== inv.length) return false;
-  else {
-    for (let i = 0; i < inv.length; i++)
-      if (inv[i] !== stringdois.reverse()[i]) {
-        return false;
-      } else {
-        return true;
-      }
-  }
+  const alfanumericos = str.toLowerCase().match(/[a-z0-9]/g);
+  return alfanumericos.join() === alfanumericos.reverse().join();
 }
 
-console.log(palindrome("not a palindrome"));
+console.log(palindrome("j-o)a||o/-oa:,o)(_j"));
